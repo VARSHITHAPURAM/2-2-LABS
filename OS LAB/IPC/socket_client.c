02478d38806f015006f5e79870b13f86459d1c55
VARSHITHAPURAM <93641265+VARSHITHAPURAM@users.noreply.github.com>
2022-06-06 12:00:47 +0530

    Update socket_client.c

--- OS LAB/IPC/socket_client.c
+++ OS LAB/IPC/socket_client.c
@@ -15,22 +15,13 @@ int main(int argc, char const* argv[])
         printf("\n Socket creation error \n");
         return -1;
     }
- 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
- 
-    // Convert IPv4 and IPv6 addresses from text to binary
-    // form
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)
-        <= 0) {
-        printf(
-            "\nInvalid address/ Address not supported \n");
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
         return -1;
     }
- 
-    if (connect(sock, (struct sockaddr*)&serv_addr,
-                sizeof(serv_addr))
-        < 0) {
+    if (connect(sock, (struct sockaddr*)&serv_addr,sizeof(serv_addr))< 0) {
         printf("\nConnection Failed \n");
         return -1;
     }
